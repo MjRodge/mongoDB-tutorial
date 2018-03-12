f69ea7fcae57917a72ff393fa6680621d3492060
Michael Rodgers <mail@mjrodge.me>
2018-03-12 10:41:34 -0400

    Exercise 4 of 9
    Here we will learn how to search for documents but only fetch the
    fields we need. Also known as projection in MongoDB

---
+++ findProject.js
@@ -0,0 +1,21 @@
+const mongo = require('mongodb').MongoClient;
+var minAge = parseInt(process.argv[2]);
+var url = "mongodb://localhost:27017/learnyoumongo";
+
+mongo.connect(url, function(err, db) {
+  if (err) {
+    console.log(err);
+  }
+  var parrots = db.collection("parrots");
+  parrots.find({
+    age: {$gt: minAge}
+  }, {
+    _id: 0 //exclude
+  }).toArray(function(err, ageData) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(ageData);
+    db.close();
+  });
+})
